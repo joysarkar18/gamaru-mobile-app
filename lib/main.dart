5be499952c426f7671de314bbe1cf146880e43bb
Joy Sarkar <joysarkar8171@gmail.com>
2023-07-20 07:35:18 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gamaru_mobile_app/Screens/home.dart';
+import 'package:gamaru_mobile_app/Screens/login_page.dart';
 import 'package:get/get.dart';
 
 import 'Controllers/Login-Contollers/autehntication.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const Login(),
     );
   }
 }
