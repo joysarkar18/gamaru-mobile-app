b88400b96e48e0f791f26083a0998646ce6c2c75
Joy Sarkar <joysarkar8171@gmail.com>
2023-08-02 00:51:23 +0530

    home page update

--- lib/Screens/Home-Screen/homeScreen.dart
+++ lib/Screens/Home-Screen/homeScreen.dart
@@ -1,6 +1,8 @@
 import 'package:another_carousel_pro/another_carousel_pro.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 import '../Top-section-every/top-part.dart';
 
@@ -26,29 +28,59 @@ class _HomeScreenState extends State<HomeScreen> {
             child: ListView(children: [
               Container(
                 padding: EdgeInsets.symmetric(
-                  horizontal: 10,
+                  horizontal: 5,
                 ),
-                height: 90,
+                height: 85,
                 width: Get.width,
                 child: AnotherCarousel(
                   images: [
-                    AssetImage("Assets/ref_banner.png"),
-                    AssetImage("Assets/customer_banner.png"),
-                    // AssetImage("Assets/ref_banner.png"),
+                    AssetImage("Assets/refer_cover_home.png"),
+                    AssetImage("Assets/service.jpeg"),
                   ],
                   showIndicator: false,
+                  onImageChange: (p0, p1) {
+                    print("${p1}");
+                  },
                   borderRadius: true,
                 ),
               ),
               SizedBox(
-                height: 20,
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 10),
+                    child: const Text(
+                      " GAMES FOR YOU",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  LottieBuilder.asset(
+                    "Assets/fire.json",
+                    height: 30,
+                    frameRate: FrameRate.max,
+                  )
+                ],
               ),
               Container(
-                height: 240,
+                height: 220,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                          "Assets/bgmi_home.png",
+                        ),
+                        fit: BoxFit.cover)),
+              ),
+              Container(
+                height: 200,
                 decoration: BoxDecoration(
                     image: DecorationImage(
-                        image: AssetImage("Assets/bgmi_home.png"),
-                        fit: BoxFit.cover)),
+                  image: AssetImage("Assets/FreeFire_home.png"),
+                )),
               )
             ]),
           )
