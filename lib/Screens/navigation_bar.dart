8e856f40d1e789f9919a1cbf0ccd863d592eabe0
Joy Sarkar <joysarkar8171@gmail.com>
2023-08-08 23:51:38 +0530

    update

--- lib/Screens/navigation_bar.dart
+++ lib/Screens/navigation_bar.dart
@@ -13,7 +13,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _currentIndex = 0;
+  int _currentIndex = 1;
 
   List<Widget> _pages = [
     EarnScreen(),
